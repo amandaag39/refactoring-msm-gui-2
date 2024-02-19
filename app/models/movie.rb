109953ec6ac3340b1e5618d9c8e2848a5433a869
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-19 19:36:58 +0000

    refactored methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,13 +16,23 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
-  def director
-    key = self.director_id
+  has_many :characters, class_name: "Character", foreign_key: "character_id"
 
-    matching_set = Director.where({ :id => key })
+  # def characters
+  #   my_id = self.id
 
-    the_one = matching_set.at(0)
+  #   matching_characters = Character.where({ :movie_id => my_id })
+  #   return matching_characters
+  # end
 
-    return the_one
-  end
+  belongs_to(:director)
+  # def director
+  #   key = self.director_id
+
+  #   matching_set = Director.where({ :id => key })
+
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
